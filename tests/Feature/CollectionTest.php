01ac8e545275b809c3c799d044bfb7d3101b0676
mursidin14 <54929754+mursidin14@users.noreply.github.com>
2024-04-28 16:02:33 +0800

    collection

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertEqualsCanonicalizing;
+
 class CollectionTest extends TestCase
 {
     public function testCollection()
@@ -20,5 +23,16 @@ class CollectionTest extends TestCase
         foreach($collection as $key => $value) {
             self::assertEquals($key + 1, $value);
         }
+    }
+
+    public function testCrud()
+    {
+        $collection = collect([]);
+        $collection->push(1, 2, 3);
+        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+
+        $result = $collection->pop();
+        assertEquals(3, $result);
+        $this->assertEqualsCanonicalizing([1, 2], $collection->all());
     }
 }
