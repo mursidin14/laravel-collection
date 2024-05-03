d9df7353606b42c8fe726b2ec526d90d50f7476c
mursidin14 <54929754+mursidin14@users.noreply.github.com>
2024-05-03 23:38:39 +0800

    implement take and skip in collection

--- tests/Feature/SlicingTest.php
+++ tests/Feature/SlicingTest.php
@@ -19,4 +19,40 @@ class SlicingTest extends TestCase
         $this->assertEqualsCanonicalizing([4, 5], $result2->all());
     }
 
+    public function testTake()
+    {
+        $collection = collect([1, 2, 3, 4, 5]);
+        $result = $collection->take(4);
+        $this->assertEqualsCanonicalizing([1, 2, 3, 4], $result->all());
+
+        $result = $collection->takeUntil(function($value, $key) {
+            return $value == 2;
+        });
+        $this->assertEqualsCanonicalizing([1], $result->all());
+
+        $result = $collection->takeWhile(function($value, $key) {
+            return $value < 4;
+        });
+        $this->assertEqualsCanonicalizing([1, 2, 3], $result->all());
+
+    }
+
+    public function testSkip()
+    {
+        $collection = collect([1, 2, 3, 4, 5]);
+        $result = $collection->skip(3);
+        $this->assertEqualsCanonicalizing([4, 5], $result->all());
+
+        $result = $collection->skipUntil(function($value, $key) {
+            return $value == 2;
+        });
+        $this->assertEqualsCanonicalizing([2, 3, 4, 5], $result->all());
+
+        $result = $collection->skipWhile(function($value, $key) {
+            return $value < 4;
+        });
+        $this->assertEqualsCanonicalizing([4, 5], $result->all());
+
+    }
+
 }
